0e384290311b8f5eddda91869a29aee9b69c1e86
HamBeomJoon <hbj0802@naver.com>
2024-05-28 00:30:38 +0900

    rememberSaveable, callBack으로 상태변경, 저장

--- FirstCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ FirstCompose/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,12 +11,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,17 +34,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ImageCard()
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+
+            ImageCard(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFavorite = isFavorite,
+            ) { favorite ->
+                isFavorite = favorite
+            }
         }
     }
 }
 
 @Composable
-fun ImageCard() {
+fun ImageCard(
+    modifier: Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit,
+) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth(0.5f)
-            .padding(16.dp),
+        modifier = modifier,
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 5.dp
@@ -57,9 +76,11 @@ fun ImageCard() {
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.TopEnd
             ) {
-                IconButton(onClick = { }) {
+                IconButton(onClick = {
+                    onTabFavorite.invoke(!isFavorite)
+                }) {
                     Icon(
-                        imageVector = Icons.Default.FavoriteBorder,
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                         contentDescription = "favorite",
                         tint = Color.White
                     )
