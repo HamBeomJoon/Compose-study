83623ad06de691b397a6586aa6cac48b12682031
HamBeomJoon <hbj0802@naver.com>
2024-06-03 00:33:54 +0900

    ViewModel Text변경

--- ViewModel/app/src/main/java/com/example/viewmodelex/MainActivity.kt
+++ ViewModel/app/src/main/java/com/example/viewmodelex/MainActivity.kt
@@ -7,31 +7,45 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-
 import androidx.compose.material3.Text
-
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<MainViewModel>()
+
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
-                    "Hello",
+                    viewModel.data.value,
                     fontSize = 30.sp,
                 )
                 Button(onClick = {
+                    viewModel.changeValue()
                 }) {
                     Text("변경")
                 }
             }
         }
+    }
+}
+
+class MainViewModel : ViewModel() {
+    private val _data = mutableStateOf("Hello")
+    val data: State<String> = _data
+
+    fun changeValue() {
+        _data.value = "World"
     }
 }
