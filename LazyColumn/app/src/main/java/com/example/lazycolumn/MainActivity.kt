da64dcc192e56eb418a6763668510ccd5410e5c5
HamBeomJoon <hbj0802@naver.com>
2024-06-05 00:38:45 +0900

    lazyColumn 예제

--- LazyColumn/app/src/main/java/com/example/lazycolumn/MainActivity.kt
+++ LazyColumn/app/src/main/java/com/example/lazycolumn/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.lazycolumn
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.ui.unit.dp
-import com.example.lazycolumn.ui.theme.LazyColumnTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +21,13 @@ class MainActivity : ComponentActivity() {
                 contentPadding = PaddingValues(all = 12.dp),
                 verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
-                items(items = getAllData) { person ->
+                itemsIndexed(
+                    items = getAllData,
+                    key = { _, person ->
+                        person.id
+                    }
+                ) { index, person ->
+                    Log.d("MainActivity", index.toString())
                     CustomItem(person = person)
                 }
             }
