5529b5e0a4cb70854555799f55d45b566aa49891
HamBeomJoon <hbj0802@naver.com>
2024-09-04 11:52:31 +0900

    init Project

--- FirstCompose/README.md
+++
@@ -1,1 +0,0 @@
-![aa](https://github.com/HamBeomJoon/Compose-study/assets/37996727/e7fd6561-7466-41f8-8e28-eaf410b0e552)
--- LazyColumn/app/src/main/java/com/example/lazycolumn/Person.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lazycolumn
-
-data class Person(
-    val id: Int,
-    val firstName: String,
-    val lastName: String,
-    val age: Int,
-)
