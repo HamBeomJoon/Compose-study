05746cf57efeb47249eb8b294385b4dd2d497122
HamBeomJoon <hbj0802@naver.com>
2024-09-12 14:43:49 +0900

    [init] AnimateCodelab Project

--- playground/material3/app/src/main/java/com/example/playground/BackdropScaffoldSample.kt
+++ playground/material3/app/src/main/java/com/example/playground/BackdropScaffoldSample.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -44,6 +45,7 @@ fun BackdropScaffoldSample() {
 
     BackdropScaffold(
         scaffoldState = scaffoldState,
+        backLayerBackgroundColor = MaterialTheme.colorScheme.primary,
         appBar = {
             TopAppBar(
                 title = { Text("Backdrop Scaffold Sample") },
@@ -73,7 +75,6 @@ fun BackdropScaffoldSample() {
                     }
                 },
                 elevation = 0.dp,
-                backgroundColor = Color.Transparent
             )
         },
         backLayerContent = {
